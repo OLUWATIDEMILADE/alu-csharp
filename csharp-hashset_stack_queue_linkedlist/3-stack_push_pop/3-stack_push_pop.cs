0abae28ed67fc40b614264fcf48e737a29a2877c
OLUWATIDEMILADE <o.olorunsol@alustudent.com>
2024-09-27 14:04:56 -0700

    2

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack<T>
+{
+    private List<T> stackList = new List<T>();
+
+    public int Count()
+    {
+        return stackList.Count;
+    }
+
+    public void Push(T obj)
+    {
+        stackList.Add(obj);
+    }
+
+    public T Pop()
+    {
+        if (stackList.Count == 0)
+        {
+            return default(T);
+        }
+        T item = stackList[stackList.Count - 1];
+        stackList.RemoveAt(stackList.Count - 1);
+        return item;
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        MyStack<int> stack = new MyStack<int>();
+        stack.Push(1);
+        Console.WriteLine(stack.Count());
+    }
+}
